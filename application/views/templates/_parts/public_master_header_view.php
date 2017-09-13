6a8a162c9e0020a6ea7e8af921db3310a1478702
su <minhsu57@gmail.com>
2017-09-14 00:20:32 +0700

    su su

--- application/views/templates/_parts/public_master_header_view.php
+++ application/views/templates/_parts/public_master_header_view.php
@@ -101,34 +101,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
         <ul class="nav navbar-nav c-nav-left">
             <li><a href="<?php echo base_url(); ?>"><i class="fa fa-home"></i></a></li>
-            <li><a href="#">Giới thiệu<span class="sr-only">(current)</span></a></li>
+            <li><a href="<?php echo base_url('gioi-thieu'); ?>">Giới thiệu<span class="sr-only">(current)</span></a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Khóa học luyện nói tiếng Anh<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="<?php echo base_url('luyen-noi-tieng-anh-nhom-4-5-nguoi'); ?>">Luyện nói nhóm 4-5 bạn cùng 100% GVNN</a></li>
-                    <li><a href="<?php echo base_url('luyen-noi-tieng-anh-nhom-8-nguoi'); ?>">Luyện nói nhóm 8 bạn cùng 100% GVNN</a></li>
+                    <li><a href="<?php echo base_url('english/luyen-noi-nhom-4-5-ban-cung-100-gvnn'); ?>">Luyện nói nhóm 4-5 bạn cùng 100% GVNN</a></li>
+                    <li><a href="<?php echo base_url('english/luyen-noi-nhom-8-ban-cung-100-gvnn'); ?>">Luyện nói nhóm 8 bạn cùng 100% GVNN</a></li>
                 </ul>
             </li>
-            <li><a href="<?php echo base_url('luyen-noi-tieng-anh-tai-cong-ty'); ?>">Khóa luyện nói tiếng Anh tại công ty</a></li>
+            <li><a href="<?php echo base_url('english/khoa-luyen-noi-tieng-anh-tai-cong-ty'); ?>">Khóa luyện nói tiếng Anh tại công ty</a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Khóa luyện thi IELTS<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="<?php echo base_url('luyen-thi-tieng-anh-speaking-writing'); ?>">Luyện thi SPEAKING & WRITING</a></li>
-                    <li><a href="<?php echo base_url('luyen-thi-tieng-anh-reading-listening'); ?>">Luyện thi READING & LISTENING</a></li>
-                    <li><a href="<?php echo base_url('luyen-thi-tieng-anh-4-ki-nang-ielts'); ?>">Luyện thi 4 kĩ năng IELTS</a></li>
+                    <li><a href="<?php echo base_url('english/luyen-thi-tieng-anh-speaking-writing'); ?>">Luyện thi SPEAKING & WRITING</a></li>
+                    <li><a href="<?php echo base_url('english/luyen-thi-tieng-anh-reading-listening'); ?>">Luyện thi READING & LISTENING</a></li>
+                    <li><a href="<?php echo base_url('english/luyen-thi-4-ki-nang-ielts'); ?>">Luyện thi 4 kĩ năng IELTS</a></li>
                 </ul>
             </li>
             <li class="dropdown">
-                <a href="<?php echo base_url('thi-thu-ielts'); ?>">Thi thử IELTS</a>
-                <!--<ul class="dropdown-menu">
-                    <li><a href="#">IELTS 5.0</a></li>
-                    <li><a href="#">IELTS 6.0</a></li>
-                    <li><a href="#">IELTS 7.0</a></li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="#">IELTS MASTER OVER SKILL</a></li>
-                </ul>-->
+                <a href="<?php echo base_url('english/thi-thu-ielts'); ?>">Thi thử IELTS</a>
             </li>
-            <li><a href="<?php echo base_url('contact'); ?>">Liên hệ</a></li>
+            <li><a href="<?php echo base_url('lien-he'); ?>">Liên hệ</a></li>
         </ul>
         <ul class="nav navbar-nav">
         </ul>
